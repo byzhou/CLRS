dbd06c4c6e35c2bc517f3f0ff23c8a5277179d22
byzhou <bobzhou@bu.edu>
2014-09-09 16:04:51 -0400

    AUTOCOMMIT ON CHANGE Tue Sep  9 16:04:51 EDT 2014

--- cha2/sort.cpp
+++ cha2/sort.cpp
@@ -1,6 +1,18 @@
-#include<iosteam>
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
 using namespace std;
 
 int main(){
+    const int num = 100000;
+    srand(time(0));
+
+    int array[num];
+
+    for (int i = 0; i < num; i++){
+        array[i]=rand();
+    }
     cout<<"The array has been sorted."<<endl;
+    return 0;
 }
