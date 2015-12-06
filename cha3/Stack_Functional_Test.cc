c68a5019f36e36dc0222dbb899747deb383e8f03
bob-icsg <bobzhou@bu.edu>
2015-12-06 16:04:00 -0500

    pop needs to debug

--- cha3/Stack_Functional_Test.cc
+++ cha3/Stack_Functional_Test.cc
@@ -11,17 +11,28 @@ int main ( int argc, char** argv ) {
 
 	 // push size checker
 	int i = 1 ;
-	for ( ; i < 10000 ; i ++ ) {
+	printf ( "Push Process\n" ) ;
+	for ( ; i < 10 ; i ++ ) {
 		stack.push (i) ;
 		if ( stack.stackSize() != i ) {
 			cerr << "stack size is wrong in push" << endl ;
+			printf ( "i value is %d\n" , i ) ;
+			printf ( "Real stack size is %d\n", stack.stackSize() ) ;
 		}
 	}
 
-	 // pop size checker
-	 for ( ; i >= 1 ; i -- ) {
-	 	if ( i != stack.pop () || i != ( stack.stackSize() + 1 ) ) 
+	// pop size checker
+	printf ( "Pop Process\n" ) ;
+	for ( ; i >= 1 ; i -- ) {
+		int popvalue = stack.pop() ;
+		if ( i != stack.pop () || i != ( stack.stackSize() + 1 ) ) {
 			cerr << "stack size is wrong in pop" << endl ;
-	 }
+			printf ( "i value is %d\n" , i ) ;
+			printf ( "popvalue value is %d\n" , popvalue ) ;
+			printf ( "Real stack size is %d\n", stack.stackSize() ) ;
+		}
+	}
+
+	 stack.pop() ;
 }
 
